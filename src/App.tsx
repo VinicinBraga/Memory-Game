6cb46117ce7fbcd3c7cc4ab5a4511566dcb4153a
VinicinBraga <vinicius@bragamatta.com.br>
2023-03-06 19:33:26 -0300

    Board n Card Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
-import './App.css'
-import Board from './components/Board'
+import React, { useState } from 'react';
+import './App.css';
+import Board from './components/Board';
+
+const cards = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L'];
 
 function App() {
+  const [selectedCards, setSelectedCards] = useState<number[]>([]);
 
+  function handleCardClick(index: number) {
+    setSelectedCards((prevSelectedCards) => [...prevSelectedCards, index]);
+  }
 
   return (
     <div className="App">
-     <h1>Memory Game</h1>
-      <Board cards={cards} onCardClick={(index) => this.handleCardClick(index)} />
+      <h1>Memory Game</h1>
+      <Board cards={cards} onCardClick={handleCardClick} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
