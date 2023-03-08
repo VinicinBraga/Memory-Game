0972803927e43a8ba59d189e31faa672fa02e969
VinicinBraga <vinicius@bragamatta.com.br>
2023-03-08 18:15:15 -0300

    Game structure Done

--- src/components/Cards/Card/Card.tsx
+++ src/components/Cards/Card/Card.tsx
@@ -1,28 +1,36 @@
 import React, { useState } from 'react';
 import './Card.css';
 
+import cardImg from '../../../assets/cards-back.png'
+
 interface CardProps {
+  setNumerosArray: React.Dispatch<React.SetStateAction<number[]>>;
+  numerosArray: number[];
   num: number;
 }
 
-const Card: React.FC<CardProps> = ({ num }) => {
+const Card: React.FC<CardProps> = ({ num, setNumerosArray, numerosArray }) => {
   const [isFlipped, setIsFlipped] = useState(false);
   const [showFront, setShowFront] = useState(true);
+  const [numero, setNumero] = useState<number>(num);
+  const frontClass = showFront ? 'card-front' : 'card-front card-hidden card-show';
+  const backClass = showFront ? 'card-back card-hidden card-show' : 'card-back';
 
+  let flippedClass = isFlipped ? 'card-flipped' : '';
+  
   const handleClick = () => {
     setIsFlipped(!isFlipped);
     setShowFront(!showFront);
+    setNumero(num)
+    setNumerosArray([...numerosArray, numero]);
   };
 
-  const frontClass = showFront ? 'card-front' : 'card-front card-hidden';
-  const backClass = showFront ? 'card-back card-hidden' : 'card-back';
-
-  const flippedClass = isFlipped ? 'card-flipped' : '';
+ 
 
   return (
     <div className={`card ${flippedClass}`} onClick={handleClick}>
       <div className={frontClass}>
-        <img src="https://picsum.photos/200/300" alt="card-back" />
+        <img src={cardImg} alt="card-back" />
       </div>
       <div className={backClass}>
          <h1>{num}</h1>
--- src/components/Cards/Cards.tsx
+++ src/components/Cards/Cards.tsx
@@ -1,5 +1,7 @@
+import React, { useState } from 'react';
 import Card from './Card/Card';
 import './Cards.css'
+
 
 const shuffle = (array: any[]) => {
   let currentIndex = array.length,randomIndex;
@@ -18,11 +20,15 @@ const numbers: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8
 const shuffledNumbers = shuffle(numbers);
 
 
+
 const Cards: React.FC = () => {
+  const [numerosArray, setNumerosArray] = useState<number[]>([]);
+    console.log(numerosArray)
+
   return (
     <div className='cards-container'>
-      {shuffledNumbers.map((num: number) => (
-        <Card num={num} key={num} />
+      {shuffledNumbers.map((num: number, index) => (
+        <Card num={num} key={index} setNumerosArray={setNumerosArray} numerosArray={numerosArray} />
       ))}
     </div>
   );
